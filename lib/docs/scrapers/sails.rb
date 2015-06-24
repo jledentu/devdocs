61a922a06ed8df314ee40780a2c2c352b2cef644
Jérémie Ledentu <jledentu@gmail.com>
2015-06-24 12:48:44 +0200

    Load Sails doc from Github rather than sailsjs.org

--- lib/docs/scrapers/sails.rb
+++ lib/docs/scrapers/sails.rb
@@ -1,33 +1,32 @@
 module Docs
   class Sails < UrlScraper
-    include StubRootPage
 
     self.name = 'Sails.js'
     self.slug = 'sails'
     self.type = 'sails'
     self.version = '0.11'
-    self.base_url = 'http://sailsjs.org/templates/reference/'
+    self.base_url = 'https://github.com/balderdashy/sails-docs/'
+    self.root_path = 'tree/master/reference'
     self.links = {
       home: 'http://sailsjs.org/',
       code: 'https://github.com/balderdashy/sails'
     }
 
-    html_filters.insert_before 'normalize_urls', 'sails/fix_urls'
     html_filters.push 'sails/clean_html', 'sails/entries'
 
     options[:title] = 'Sails.js'
-    options[:skip_links] = false
+    options[:container] = '.repository-content'
+
+    options[:only_patterns] = [/tree\/master\/reference/, /blob\/master\/reference/]
+
+    options[:fix_urls] = ->(url) do
+      url.sub! '/blob/master/', '/tree/master/'
+      url
+    end
 
     options[:attribution] = <<-HTML
       &copy; 2015 Mike McNeil and Balderdash Design Co.<br>
       Licensed under the MIT License.
     HTML
-
-    def root_page_body
-      require 'capybara'
-      Capybara.current_driver = :selenium
-      Capybara.visit(base_url + '/#!/documentation/reference/')
-      Capybara.find('.reference-sidebar')['innerHTML']
-    end
   end
 end
